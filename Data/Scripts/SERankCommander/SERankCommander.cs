26728b60b293393ecdf6aadbd9573db72c7321f8
generalwrex <bjdroot@gmail.com>
2014-09-20 21:17:21 -0700

    -Initial commit of the visual studio solution of SERankCommander

---
+++ Data/Scripts/SERankCommander/SERankCommander.cs
@@ -0,0 +1,39 @@
+﻿/*
+		https://github.com/SE-InfinitySpaceMods
+ 
+		©SE-InfinitySpaceMods
+		2014
+ 
+ 
+		SE-RankCommander 
+ 
+		generalwrex
+        XxDaOverlordxX
+		meatsauce007
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Common.ObjectBuilders.Definitions;
+
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces;
+
+using VRage;
+using VRage.Serialization;
+using VRageMath;
+
+
+namespace SERankCommander
+{
+    public class SeRankCommander
+    {
+		// woo this will have some very good stuff in it
+    }
+}
